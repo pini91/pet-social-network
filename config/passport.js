6c233b7dcf02f15a5d3300476368727082615f2b
pini91 <brenda.loncaric@gmail.com>
2025-07-01 18:33:45 -0400

    went back to the main layout

--- config/passport.js
+++ config/passport.js
@@ -35,7 +35,12 @@ module.exports = function (passport) {
     done(null, user.id);
   });
 
-  passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findById(id);
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
   });
 };
